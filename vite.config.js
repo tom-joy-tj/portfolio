757bdc02576b3dc8c997a8bd22cf5fa1b2b5ebd7
tom joy <thomaspjoy1@gmail.com>
2025-05-29 00:08:19 +0100

    added vite config

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/portfolio/', // adjust to '/your-repo-name/' if deployed from a project repo
+})
