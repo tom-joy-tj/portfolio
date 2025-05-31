153227baa39975b44647c4a278712bfc2e759f26
tom joy <thomaspjoy1@gmail.com>
2025-05-31 01:48:57 +0100

    added images to header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,14 @@
 import github from "../assets/gh_logo.png"
+import linkedIn from "../assets/li_logo.png"
+import email from "../assets/email_logo.png"
 
 const Header = () => {
   return (
     <header>
-        <img src={github} className="headerLogo" />
       <nav>
-        <a href="#about">About</a> |{" "}
-        <a href="#projects">Projects</a> |{" "}
-        <a href="#contact">Contact</a>
+       <img src={github} className="headerLogo" />
+       <img src={linkedIn} className="headerLogo" />
+       <img src={email} className="headerLogo" />
       </nav>
     </header>
   );
