e5573d187e358aefc6e8a00f975f80f7621372d4
tom joy <thomaspjoy1@gmail.com>
2025-05-31 01:00:53 +0100

    built main component

---
+++ components/Main.jsx
@@ -0,0 +1,7 @@
+const Main = () => {
+    return (
+        <img src="assets/signature_black_vector.png"></img>
+    );
+};
+
+export default Main;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Main from '../components/Main';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <div>
         <Header />
         <main>
+        <Main/>
         </main>
       </div>
     )
